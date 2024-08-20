3540e64580c88954a9bd284c759a22acad7578c2
viktoria <vvikulya.udalova@mail.ru>
2024-08-20 18:31:19 +0700

    Добавляет правки

--- js/main.js
+++ js/main.js
@@ -16,21 +16,8 @@
 // Для формирования текста комментария — message — вам необходимо взять одно или два случайных предложения из представленных ниже:
 // Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте случайное имя в поле name.
 
-const ID = '';
-const URL = '';
-const DESCRIPTION  = ['Я на Мальдивах', 'Снова вместе', 'Кушоем сосиску', 'Мда', 'ЧТО ЖЕ ДЕЛАТЬ', 'Собака сошла с ума', 'Пью пиво']
-const LIKES = '';
-const COMMENTS = [{
-  id: '',
-  avatar: '',
-  message: ['Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'],
-  name: ['Тася Пупкина', 'Аврора Иванова', 'Федя Печенькин', 'Дмитрий Лже', 'Евраким Автодов', 'Вова Гаечкин', 'Шала Гаврилова', 'Степанина']
-}]
+const DESCRIPTION = ['Я на Мальдивах', 'Снова вместе', 'Кушоем сосиску', 'Мда', 'ЧТО ЖЕ ДЕЛАТЬ', 'Собака сошла с ума', 'Пью пиво'];
+const COMMENTS = [];
 
 const SIMILAR_USERS_COUNT = 25;
 
@@ -43,25 +30,32 @@ const getRandomInteger = (a, b) => {
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const createComment = function () {
-  const deep = JSON.parse(JSON.stringify(COMMENTS[0]))
-  deep.id = getRandomInteger(0, 300)
-  deep.avatar = 'img/avatar-' + getRandomInteger(0, 6) + '.svg'
-  deep.message = getRandomArrayElement(COMMENTS[0].message)
-  deep.name = getRandomArrayElement(COMMENTS[0].name)
-  return deep
-}
+const newComment = () => {
+  let comment = COMMENTS[0];
+  comment = {
+    id: getRandomInteger(0, 300),
+    avatar: `img/avatar-${ getRandomInteger(0, 6) }.svg`,
+    message: getRandomArrayElement(['Всё отлично!',
+      'В целом всё неплохо. Но не всё.',
+      'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+      'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+      'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+      'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!']),
+    name: getRandomArrayElement(['Тася Пупкина', 'Аврора Иванова', 'Федя Печенькин', 'Дмитрий Лже', 'Евраким Автодов', 'Вова Гаечкин', 'Шала Гаврилова', 'Степанина']),
+  };
+  return comment;
+};
 
-const createUser = function() {
-  newObj = {};
-  newObj.id = getRandomInteger(1, 25),
-  newObj.url = 'photos/' + getRandomInteger(1, 25) + '.jpg',
-  newObj.description = DESCRIPTION[getRandomInteger(0, DESCRIPTION.length - 1)],
-  newObj.likes = getRandomInteger(15, 200),
-  newObj.comment = Array.from({length: getRandomInteger(0, 30)}, createComment)
-  return newObj
-}
+const createPost = () => {
+  let newObj = {};
+  newObj = {
+    id: getRandomInteger(1, 25),
+    url: `photos/${ getRandomInteger(1, 25) }.jpg`,
+    description: DESCRIPTION[getRandomInteger(0, DESCRIPTION.length - 1)],
+    likes: getRandomInteger(15, 200),
+    comment: Array.from({length: getRandomInteger(0, 30)}, newComment)
+  };
+  return newObj;
+};
 
-const similarUsers = Array.from({length: SIMILAR_USERS_COUNT}, createUser);
-
-console.log(JSON.stringify(similarUsers, null, 2))
+export const similarUsers = Array.from({length: SIMILAR_USERS_COUNT}, createPost);
