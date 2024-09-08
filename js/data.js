62b00307e234b6f885fd52834bbe120d45d4d726
viktoria <vvikulya.udalova@mail.ru>
2024-09-08 18:33:30 +0700

    добавляет правки

--- js/data.js
+++ js/data.js
@@ -1,20 +1,11 @@
-import {getRandomArrayElement} from './util.js';
-import { getRandomInteger } from './util.js';
-
-const DESCRIPTION = ['Я на Мальдивах', 'Снова вместе', 'Кушоем сосиску', 'Мда', 'ЧТО ЖЕ ДЕЛАТЬ', 'Собака сошла с ума', 'Пью пиво'];
-
-const SIMILAR_USERS_COUNT = 25;
+import { getRandomArrayElement, getRandomInteger } from './util.js';
+import {DESCRIPTION, NAMES, SIMILAR_USERS_COUNT, MESSAGES} from './const.js';
 
 const createNewComment = () => ({
   id: getRandomInteger(0, 300),
   avatar: `img/avatar-${ getRandomInteger(0, 6) }.svg`,
-  message: getRandomArrayElement(['Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!']),
-  name: getRandomArrayElement(['Тася Пупкина', 'Аврора Иванова', 'Федя Печенькин', 'Дмитрий Лже', 'Евраким Автодов', 'Вова Гаечкин', 'Шала Гаврилова', 'Степанина']),
+  message: getRandomArrayElement(MESSAGES),
+  name: getRandomArrayElement(NAMES),
 }
 );
 
@@ -26,6 +17,4 @@ const createPost = () => ({
   comment: Array.from({length: getRandomInteger(0, 30)}, createNewComment)
 });
 
-const similarUsers = Array.from({length: SIMILAR_USERS_COUNT}, createPost);
-
-export {similarUsers};
+export const similarUsers = Array.from({length: SIMILAR_USERS_COUNT}, createPost);
--- js/main.js
+++ js/main.js
@@ -1,1 +1,2 @@
 import { similarUsers } from './data.js';
+
